0590740c5a1851fb5299be5a0e841482949d8e10
Andrew Grin <andgrindev@icloud.com>
2022-07-28 17:43:20 +0300

    Add some separation

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-const server = http.createServer(app);
+const adminRouters = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+// add parsing all the data come in Req.body
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
-server.listen(3000);
+// use adminRouters as middleware
+app.use(adminRouters);
+//  -/-
+app.use(shopRoutes);
+
+app.listen(3000);
