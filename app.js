0f4b06565499ff4458cf27474a47e1754b661d66
Andrew Grin <andgrindev@icloud.com>
2022-07-28 18:29:16 +0300

    Add 404 page

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -17,7 +18,7 @@ app.use('/admin', adminRouters);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page not found</h1>');
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
