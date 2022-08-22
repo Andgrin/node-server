e9bd1925027c32c9fb33479a38b74aeab412826b
Andrew Grin <andgrindev@icloud.com>
2022-08-22 11:56:13 +0300

    add MVC

--- app.js
+++ app.js
@@ -1,30 +1,26 @@
-const http = require('http');
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const errorController = require('./controllers/error');
+
 const app = express();
 
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRouters = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 // add parsing all the data come in Req.body
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(bodyParser.urlencoded({ extended: false }));
 // share static files like CSS styles
 app.use(express.static(path.join(__dirname, 'public')));
 
 // use adminRouters as middleware
-app.use('/admin', adminRouters.router);
+app.use('/admin', adminRouters);
 //  -/-
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-});
-// module.exports = path.dirname(require.main.filename);
+app.use(errorController.get404);
 
 app.listen(3000);
---
+++ controllers/products.js
@@ -0,0 +1,32 @@
+// const products = [];
+
+const Product = require('../models/product');
+
+exports.getAddProduct = (req, res, next) => {
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
+};
+
+exports.postAddProduct = (req, res, next) => {
+  const product = new Product(req.body.title);
+  product.save();
+  // products.push({ title: req.body.title });
+  res.redirect('/');
+}
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render('shop', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      formsCSS: true,
+      productCSS: true
+    });
+  });
+};
